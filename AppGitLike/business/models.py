1edea7df2685dd44d39d87c4e4614a03bb5f279a
Rogerio Silva <rogerio.silva@ifpi.edu.br>
2018-10-21 10:16:15 -0300

    #GitLike: Projeto Inicial, estrutura basica e modelos.

---
+++ AppGitLike/business/models.py
@@ -0,0 +1,34 @@
+class Repositorio:
+
+    def __init__(self, nome):
+        self.nome = nome
+        self.remotes = []
+        self.arquivos = []
+
+
+    def novo_arquivo(self, nome):
+        pass
+
+    def remover_arquivo(self, nome):
+        pass
+
+
+class Arquivo:
+    pass
+
+
+class Commit:
+    pass
+
+
+class Mudanca:
+    pass
+
+
+class StageArea:
+    pass
+
+
+class Remote:
+    pass
+
